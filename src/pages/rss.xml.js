6e0d183005cde55b7f914bcb7bd3f88680661394
Christian Viñolo <christianvf.adev@gmail.com>
2026-01-19 02:49:10 +0100

    modify tags, archive and rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
   return rss({
-    title: 'Alumno de Astro | Blog',
-    description: 'Mi viaje de aprendizaje de Astro',
+    title: 'Cheito Díaz | Noticias',
+    description: 'Actualidad, opiniones y análisis sobre el fútbol base y regional.',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+      author: post.data.author,
+      categories: post.data.tags, // RSS tags
+    })),
+    customData: `<language>es-ES</language>`,
   });
 }
